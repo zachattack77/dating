f1812df0e037270e8fa9324c9e2630710848946f
Zach Kunitsa <zkunitsa@yahoo.com>
2018-03-02 16:12:13 -0500

    added memmebr.php class

---
+++ classes/member.php
@@ -0,0 +1,32 @@
+<?php
+class Member
+{
+    protected $fname;
+    protected $lname;
+    protected $age;
+    protected $gender;
+    protected $phone;
+    protected $email;
+    protected $state;
+    protected $seeking;
+    protected $bio;
+
+    /**
+     * Member constructor.
+     * @param $fname first name of user
+     * @param $lname last name of user
+     * @param $age age of user
+     * @param $gender gender of user
+     * @param $phone phone number of user
+     */
+    public function __construct($fname, $lname, $age, $gender, $phone)
+    {
+        $this->fname = $fname;
+        $this->lname = $lname;
+        $this->age = $age;
+        $this->gender = $gender;
+        $this->phone = $phone;
+    }
+
+
+}
